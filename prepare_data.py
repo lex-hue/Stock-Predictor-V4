dba2448201b8e88d25f6ec868f0f8d4247e6522d
Qerim Iseni09 <qerimiseni0007@gmail.com>
2023-03-09 18:17:14 +0100

    Add files via upload

---
+++ prepare_data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import talib
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/BTC-USD.csv')
+
+df['SMA'] = talib.SMA(df['Close'], timeperiod=14)
+df['RSI'] = talib.RSI(df['Close'], timeperiod=14)
+df['MACD'], _, _ = talib.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+df['upper_band'], df['middle_band'], df['lower_band'] = talib.BBANDS(df['Close'], timeperiod=20)
+df['aroon_up'], df['aroon_down'] = talib.AROON(df['High'], df['Low'], timeperiod=25)
+df['kicking'] = talib.CDLKICKINGBYLENGTH(df['Open'], df['High'], df['Low'], df['Close'])
+
+df['ATR'] = talib.ATR(df['High'], df['Low'], df['Close'], timeperiod=14)
+df['upper_band_supertrend'] = df['High'] - (df['ATR'] * 2)
+df['lower_band_supertrend'] = df['Low'] + (df['ATR'] * 2)
+df['in_uptrend'] = df['Close'] > df['lower_band_supertrend']
+df['supertrend_signal'] = df['in_uptrend'].diff().fillna(0)
+
+# Save the DataFrame to a new CSV file with indicators
+df.to_csv('data.csv', index=False)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
+
+ax1.plot(df['Close'], label='Close')
+ax1.plot(df['SMA'], label='SMA')
+ax1.fill_between(df.index, df['upper_band'], df['lower_band'], alpha=0.2, color='gray')
+ax1.plot(df['upper_band'], linestyle='dashed', color='gray')
+ax1.plot(df['middle_band'], linestyle='dashed', color='gray')
+ax1.plot(df['lower_band'], linestyle='dashed', color='gray')
+ax1.scatter(df.index[df['supertrend_signal'] == 1], df['Close'][df['supertrend_signal'] == 1], marker='^', color='green', s=100)
+ax1.scatter(df.index[df['supertrend_signal'] == -1], df['Close'][df['supertrend_signal'] == -1], marker='v', color='red', s=100)
+ax1.legend()
+
+ax2.plot(df['RSI'], label='RSI')
+ax2.plot(df['aroon_up'], label='Aroon Up')
+ax2.plot(df['aroon_down'], label='Aroon Down')
+ax2.scatter(df.index[df['kicking'] == 100], df['High'][df['kicking'] == 100], marker='^', color='green', s=100)
+ax2.scatter(df.index[df['kicking'] == -100], df['Low'][df['kicking'] == -100], marker='v', color='red', s=100)
+ax2.legend()
+
+plt.xlim(df.index[0], df.index[-1])
+
+plt.show()
---
+++ train.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# Load data
+data = pd.read_csv("data.csv")
+
+# Split data into train and test sets
+train_data = data.iloc[:int(0.8*len(data))]
+test_data = data.iloc[int(0.8*len(data)):]
+
+# Normalize data
+scaler = MinMaxScaler()
+train_data_norm = scaler.fit_transform(train_data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'SMA', 'RSI', 'MACD', 'upper_band', 'middle_band', 'lower_band', 'aroon_up', 'aroon_down', 'kicking', 'ATR', 'upper_band_supertrend', 'lower_band_supertrend']])
+test_data_norm = scaler.transform(test_data[['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'SMA', 'RSI', 'MACD', 'upper_band', 'middle_band', 'lower_band', 'aroon_up', 'aroon_down', 'kicking', 'ATR', 'upper_band_supertrend', 'lower_band_supertrend']])
+
+# Define time steps
+timesteps = 100
+
+# Create sequences of timesteps
+def create_sequences(data, timesteps):
+    X = []
+    y = []
+    for i in range(timesteps, len(data)):
+        X.append(data[i-timesteps:i])
+        y.append(data[i, 3])
+    return np.array(X), np.array(y)
+
+X_train, y_train = create_sequences(train_data_norm, timesteps)
+X_test, y_test = create_sequences(test_data_norm, timesteps)
+
+# Build model
+model = Sequential()
+model.add(LSTM(units=50, input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+model.compile(optimizer='adam', loss='mse')
+
+# Define callbacks
+early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True)
+
+# Train model
+history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test), callbacks=[early_stopping, checkpoint])
+
+# Load best model
+model = tf.keras.models.load_model('best_model.h5')
+
+# Evaluate model on test data
+y_pred = model.predict(X_test)
+rmse = np.sqrt(np.mean((y_pred - y_test)**2))
+print(f"RMSE: {rmse}")
