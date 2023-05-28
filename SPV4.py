d22b36205b66c6f5362ccee9552c6b802d248466
Qerim-iseni09 <qerimiseni0007@gmail.com>
2023-05-28 15:31:19 +0200

    Better Installation Process!

--- SPV4.py
+++ SPV4.py
@@ -1,58 +1,102 @@
 import argparse
 
 def install_dependencies():
-    print("Installing all dependencies for SPV4...")
     import os
     import subprocess
     import time
     import urllib.request
     import tarfile
+    import sys
 
     def download_file(url, filename):
         print(f"Downloading {filename}...")
-        urllib.request.urlretrieve(url, filename)
-        print(f"Download complete: {filename}")
+        start_time = time.time()
+        response = urllib.request.urlopen(url)
+        file_size = int(response.headers["Content-Length"])
+        downloaded = 0
+        block_size = 8192
+        with open(filename, "wb") as file:
+            while True:
+                buffer = response.read(block_size)
+                if not buffer:
+                    break
+                file.write(buffer)
+                downloaded += len(buffer)
+                progress = downloaded / file_size * 100
+                print(f"Progress: {progress:.2f}%", end="\r")
+        print()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Download complete: {filename} (Time: {elapsed_time:.2f} seconds)")
 
     def extract_tar_gz(filename):
         print(f"Extracting {filename}...")
+        start_time = time.time()
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall()
-        print(f"Extraction complete: {filename}")
+            file_count = len(tar.getmembers())
+            extracted = 0
+            for member in tar:
+                tar.extract(member)
+                extracted += 1
+                progress = extracted / file_count * 100
+                print(f"Progress: {progress:.2f}%", end="\r")
+        print()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Extraction complete: {filename} (Time: {elapsed_time:.2f} seconds)")
 
     def install_ta_lib():
         print("Installing TA-Lib...")
+        start_time = time.time()
         os.chdir("ta-lib")
         subprocess.run(["./configure", "--prefix=/usr"], check=True)
-        subprocess.run(["make"], check=True)
-        subprocess.run(["sudo", "make", "install"], check=True)
+        subprocess.run(["make", "-s"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "make", "-s", "install"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         os.chdir("..")
-        print("TA-Lib installation complete")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"TA-Lib installation complete (Time: {elapsed_time:.2f} seconds)")
 
     def install_dependencies():
         print("Installing Python dependencies...")
-        subprocess.run(["pip", "install", "pandas", "numpy", "scikit-learn", "tensorflow-cpu", "matplotlib", "ta-lib"], check=True)
-        print("Python dependencies installation complete")
+        start_time = time.time()
+        packages = ["pandas", "numpy", "scikit-learn", "tensorflow-cpu", "matplotlib", "ta-lib"]
+        total_packages = len(packages)
+        progress = 0
+        for package in packages:
+            progress += 1
+            print(f"Installing {package}... ({progress}/{total_packages})")
+            subprocess.run([sys.executable, "-m", "pip", "install", "--quiet", package], check=True)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Python dependencies installation complete (Time: {elapsed_time:.2f} seconds)")
 
     if __name__ == "__main__":
-        print("Cancel the script if you didn't set up the virtual environment yet (Optional)")
+        print("Welcome to the SPV4 installation!")
+        print("This script will install all the necessary dependencies.\n")
+
         time.sleep(2)
-        os.system("clear")
-        print("If you did or don't want a virtual environment setup, wait 3 seconds")
-        time.sleep(3)
 
-        # Download TA-Lib
-        ta_lib_url = "https://deac-fra.dl.sourceforge.net/project/ta-lib/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz"
-        ta_lib_filename = "ta-lib-0.4.0-src.tar.gz"
-        download_file(ta_lib_url, ta_lib_filename)
+        download_file("https://deac-fra.dl.sourceforge.net/project/ta-lib/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz", "ta-lib-0.4.0-src.tar.gz")
 
-        # Extract TA-Lib
-        extract_tar_gz(ta_lib_filename)
+        print("Extraction process will begin shortly...")
+        print("Please wait while the files are being extracted.")
 
-        # Install TA-Lib
+        extract_tar_gz("ta-lib-0.4.0-src.tar.gz")
+
+        print("Extraction process completed successfully!\n")
+        print("TA-Lib installation will now begin.")
+
         install_ta_lib()
 
-        # Install Python dependencies
+        print("TA-Lib installation completed successfully!\n")
+
+        print("Python dependencies installation will now begin.")
+
         install_dependencies()
+
+        print("Python dependencies installation completed successfully!\n")
+        print("SPV4 installation completed successfully!")
 
 
 def prepare_data():
